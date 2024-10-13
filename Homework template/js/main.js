3dbbc6a2581464a3bd04dfa0dfd08c814381d5c9
Matei M <matei.mitnei@m-tag.io>
2024-10-13 19:07:21 +0300

    Finished

--- Homework template/js/main.js
+++ Homework template/js/main.js
@@ -1,138 +1,59 @@
 (function(){
-    const allImages = transformData();
+    const gallery = document.querySelector('#gallery'),
+        btn = document.querySelector('.add-image'),
+        counter = document.querySelector('.counter'),
+        modalAlert = document.querySelector('#modal-alert'),
+        sortSelect = document.querySelector('#sort-select'),
+        maxItems = 16;
+    let dataCpy = data,
+        uniqueId = data.length;
 
-    const btn = document.getElementById("play"),
-        firstBlock = document.querySelector('#first-line'),
-        secondBlock = document.querySelector('#second-line'),
-        thirdBlock = document.querySelector('#third-line'),
-        firstGroup = document.querySelector('.first-group'),
-        secondGroup = document.querySelector('.second-group'),
-        thirdGroup = document.querySelector('.third-group'),
-        methodElement = document.getElementById('type-selector'),
-        numberOfItemsElement = document.getElementById('line-selector')
+    reload();
 
-    const methodHandler = {
-        "1": generateGalleryWithReplace,
-        "2": generateGalleryWithStringTemplate,
-        "3": generateGalleryWithCreateElement,
+    // Creates the gallery and updates the image counter
+    function reload() {
+        const savedFilter = localStorage.getItem('sortMethod') || 'a-to-z';
+        sortSelect.value = savedFilter;
+        dataCpy = sortImages(dataCpy, savedFilter);
+        gallery.innerHTML = generateGallery(transformData(dataCpy));
+        counter.textContent = `${dataCpy.length} / ${maxItems}`;
+        headerStyle (dataCpy.length === maxItems);
     }
 
-    function init() {
-        // Values from the select boxes
-        const method = methodElement.value;
-        const numberOfItems = getNumberOfItems(numberOfItemsElement.value);
-
-        hideAllBlocks();
-
-        // Determine which method of building the gallery needs to be used
-        // Replace method
-        if (method === "1") {
-            firstGroup.classList.replace("hide","show");
-
-            firstBlock.innerHTML = generateGallery(method, numberOfItems);
+    function headerStyle(boolean) {
+        if (boolean) {
+            btn.id = "disabled-btn";
+            counter.id = "full-counter";
         }
-        // Strings method
-        else if (method === "2") {
-            secondGroup.classList.replace("hide","show");
-
-            secondBlock.innerHTML = generateGallery(method, numberOfItems);
-        }
-        else if (method === "3") {
-            thirdGroup.classList.replace("hide","show");
-
-            thirdBlock.innerHTML = generateGallery(method, numberOfItems);
+        else {
+            btn.id = "";
+            counter.id = "";
         }
     }
 
-    function generateGallery(method, numberOfItems) {
+    function generateGallery(allImages) {
         let result = '';
-
-        for (let index = 0; index < numberOfItems; index++) {
-            result += methodHandler[method](allImages[index])
+        for (let index = 0; index < allImages.length; index++) {
+            const item = allImages[index];
+            result +=
+                `<div class="image-wrapper blur" id="${item.id}">\
+                    <img src="${item.url}" alt="${item.name}" class="round-border">\
+                    <div class="info-wrapper">\
+                        <div>${item.name}</div>\
+                        <div class="top-padding">${item.description}</div>\
+                        <div>${item.date}</div>\
+                    </div>\
+                    <button class="delete-btn round-border blur">Delete</button>\
+                </div>`;
         }
-
         return result;
     }
 
-    function generateGalleryWithReplace(item) {
-        const replaceItems =
-            '<div class="col-lg-3 col-md-6">\
-                <img src="$url" alt="$name" class="img-thumbnail">\
-                <div class="info-wrapper">\
-                    <div class="text-muted">$name</div>\
-                    <div class="text-muted top-padding">$description</div>\
-                    <div class="text-muted">$date</div>\
-                </div>\
-            </div>';
-
-        return replaceItems
-            .replace(/\$name/gi, item.name)
-            .replace("$url", item.url)
-            .replace("$description", item.description)
-            .replace("$date", item.date);
-    }
-
-    function generateGalleryWithStringTemplate(item) {
-       return `<div class="col-sm-3 col-xs-6">\
-                        <img src="${item.url}" alt="${item.name}" class="img-thumbnail">\
-                        <div class="info-wrapper">\
-                            <div class="text-muted">${item.name}</div>\
-                            <div class="text-muted top-padding">${item.description}</div>\
-                            <div class="text-muted">${item.date}</div>\
-                        </div>\
-                    </div>`;
-    }
-
-    function generateGalleryWithCreateElement(item) {
-        const imageContainer = document.createElement("div");
-        imageContainer.classList.add("col-sm-3", "col-xs-6");
-        const image = document.createElement("img");
-        image.src = item.url;
-        image.alt = item.name;
-        image.classList.add("img-thumbnail");
-        const infoBox = document.createElement("div");
-        infoBox.classList.add("info-wrapper");
-        const title = document.createElement("div");
-        title.textContent = item.name;
-        title.classList.add("text-muted");
-        const description = document.createElement("div");
-        description.textContent = item.description;
-        description.classList.add("text-muted", "top-padding");
-        const date = document.createElement("div");
-        date.textContent = item.date;
-        date.classList.add("text-muted");
-
-        infoBox.appendChild(title);
-        infoBox.appendChild(description);
-        infoBox.appendChild(date);
-
-        imageContainer.appendChild(image);
-        imageContainer.appendChild(infoBox);
-
-        return imageContainer.outerHTML;
-    }
-
-    function getNumberOfItems(value) {
-        switch (value) {
-            case "0": // All images
-                return allImages.length;
-            case "1": // 3 images
-                return 3;
-            case "2": // 6 images
-                return 6;
-        }
-    }
-
-    function hideAllBlocks() {
-        firstGroup.classList.replace("show","hide");
-        secondGroup.classList.replace("show","hide");
-        thirdGroup.classList.replace("show","hide");
-    }
-
-    function transformData() {
-        return data.map(function(item){
+    function transformData(data) {
+        return data.map((item) => {
             const tmpDate = new Date(item.date);
             return {
+                id: item.id,
                 name : item.name[0].toUpperCase() + item.name.slice(1).toLowerCase(),
                 url : "http://" + item.url,
                 description : item.description.slice(0, 15) + "...",
@@ -145,5 +66,58 @@
         });
     }
 
-    btn.addEventListener("click", init);
+    // If possible, adds a random image from 'data', else displays the modal pop-up
+    function addImage() {
+        if (dataCpy.length < maxItems) {
+            const rndIndex = Math.floor(Math.random() * data.length);
+            const newItem = {...data[rndIndex]};
+            newItem.id = ++uniqueId;
+            dataCpy = [newItem, ...dataCpy];
+            reload();
+        }
+        else {
+            const modalPopUp = new bootstrap.Modal(modalAlert);
+            modalPopUp.show();
+        }
+    }
+
+    // Deletes the parent image of the clicked 'Delete' button
+    function deleteImage() {
+        gallery.addEventListener('click', (event) => {
+            // Check if the clicked element is a delete button
+            if (event.target.classList.contains('delete-btn')) {
+                // Remove the parent item (image-wrapper)
+                const imageItem = event.target.closest('.image-wrapper');
+                if (imageItem) {
+                    dataCpy = dataCpy.filter(item => item.id !== Number(imageItem.id));
+                    reload();
+                }
+            }
+        });
+    }
+
+    function sortImages(dataCpy, sortMethod) {
+        switch (sortMethod) {
+            case 'a-to-z':
+                return dataCpy.sort((a, b) => a.name.localeCompare(b.name));
+            case 'z-to-a':
+                return dataCpy.sort((a, b) => b.name.localeCompare(a.name));
+            case 'newest':
+                return dataCpy.sort((a, b) => b.date - a.date);
+            case 'oldest':
+                return dataCpy.sort((a, b) => a.date - b.date);
+            default:
+                return dataCpy;
+        }
+    }
+
+    function changeSort() {
+        const sortMethod = sortSelect.value;
+        localStorage.setItem('sortMethod', sortMethod);
+        reload();
+    }
+
+    sortSelect.addEventListener('change', changeSort);
+    btn.addEventListener('click', addImage);
+    document.addEventListener('DOMContentLoaded', deleteImage);
 })()
